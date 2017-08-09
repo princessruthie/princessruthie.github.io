0f4faa5b4941cf7083cc4d34a0735f5bf1bab956
Princess Ruthie <missruthie@gmail.com>
2017-08-09 17:50:40 -0400

    Add gruntfile and index.html

--- gruntfile.js
+++ gruntfile.js
@@ -11,6 +11,10 @@ module.exports = function(grunt) {
   }); //initConfig
 
   grunt.loadNpmTasks('grunt-contrib-concat');
-
-  grunt.registerTask('develop', ['this is for dev',] ,['concat',]);
+  /*shallow rabbit hole:
+  grunt tasks can be named whatever, including 'default'
+  registerTask task an optional middle parameter-- a description array
+  trailing commas are helpful in objects and array because they make adding on easier
+  // grunt.registerTask('develop', ['this is for dev',] ,['concat',]);
+  */
 };  //wrapper function
